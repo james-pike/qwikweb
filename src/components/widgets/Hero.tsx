a3df357212a0e1737b24581dd103c75dd6a775b3
James Pike <jamesandrewpike@gmail.com>
2024-11-07 13:28:44 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -1,14 +1,20 @@
-import { component$ } from "@builder.io/qwik";
-// import { Image } from "@unpic/qwik";
-import LottieAnimation from "./LottieAnimation";
+import { component$ } from '@builder.io/qwik';
+import { QwikLottie } from 'qwik-lottie';
+import { Options } from './types';
 
-// const coverImage = "https://qwikweb.vercel.app/images/mp.jpg";
+// Static import if the file is included in your build
+import animationData from 'public/animations/bluestars.json';
 
 export default component$(() => {
+  const options: Options = {
+    loop: true,
+    autoplay: true,
+    animationData,  // Use the statically imported JSON data
+  };
+
   return (
     <section class="relative min-h-[85vh] md:-mt-[76px] not-prose bg-gradient-to-r from-primary-100 to-secondary-100">
       <div class="absolute pointer-events-none" aria-hidden="true"></div>
-      {/* removed px-4 to make ring-inset effect */}
       <div class="relative max-w-7xl mx-auto px-4 sm:px-6">
         <div class="pt-0 md:pt-[76px] pointer-events-none "></div>
         <div class="py-12 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-[85vh] lg:gap-8">
@@ -24,12 +30,10 @@ export default component$(() => {
                   Comprehensive Legal Support
                 </span>{" "}
                 specializing in Divorce, Child Custody, Spousal Support, Property Division, Adoption, Mediation and more.{" "}
-                
                 <span class="hidden md:inline">
                   Dark Mode, Great Page Speed, image optimization, sitemap generation and more.
                 </span>
               </p>
-
               <div class="max-w-xs sm:max-w-md m-auto flex flex-nowrap flex-col sm:flex-row sm:justify-center gap-4 lg:justify-start lg:m-0 lg:max-w-7xl">
                 <div class="flex w-full sm:w-auto">
                   <a
@@ -48,8 +52,7 @@ export default component$(() => {
             </div>
           </div>
           <div class="basis-1/2">
-          <LottieAnimation animationData="/animations/bluestars.json" loop={false} autoplay={true} />
-
+          <script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs" type="module"></script><dotlottie-player src="https://lottie.host/fe0873a0-ab53-482e-b136-941d01a9ae1c/N5g5h1cMVf.json" background="transparent" speed="1" style="width: 300px; height: 300px" direction="1" playMode="normal" autoplay hover></dotlottie-player>
           </div>
         </div>
       </div>
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -21,8 +21,10 @@ import Header from "~/components/widgets/Header";
 export default component$(() => {
   return (
     <>
+    
       <Header />
       <main>
+      <script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs" type="module"></script> 
         <Slot />
       </main>
       <Footer />
