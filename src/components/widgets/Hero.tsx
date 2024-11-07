7e4958a70720a10430572386b50b0953a2d2a498
jvm3 <jamesandrewpike@gmail.com>
2024-11-06 19:19:50 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -1,12 +1,14 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, } from "@builder.io/qwik";
 
-import Image from '~/assets/images/mp.jpg?w=800&h=800&jsx';
+const coverImage = '~/assets/images/mp.jpg?w=400&h=400&jsx';
 
 export default component$(() => {
   return (
     <section class="relative md:-mt-[76px] not-prose bg-gradient-to-r from-primary-200 to-secondary-300">
       <div class="absolute inset-0 pointer-events-none" aria-hidden="true"></div>
+        <div className="absolute inset-4 rounded-4xl bg-white/80" />
       <div class="relative max-w-7xl mx-auto px-4 sm:px-6">
+
         <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
         <div class="py-12 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
           <div class="basis-1/2 text-center lg:text-left pb-10 md:pb-16 mx-auto">
@@ -45,16 +47,7 @@ export default component$(() => {
             </div>
           </div>
           <div class="basis-1/2">
-          <Image
-            
-              layout="constrained"
-              width={500}
-              height={500}
-              alt="Qwind Hero Image (Cool dog)"
-              class="mx-auto lg:mr-0 w-full drop-shadow-2xl rounded-md"
-              priority={true}
-              breakpoints={[320, 480, 640, 768, 1024]}
-            /> 
+
 
            
           </div>
